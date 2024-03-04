af1038238c72fd4ec33f585c9d38cf27ecaa29f0
Hussain-buzilf <154977471+Hussain-buzilf@users.noreply.github.com>
2024-03-04 09:01:46 +0000

    Responsive Header

--- src/Compnents/Header.jsx
+++ src/Compnents/Header.jsx
@@ -1,47 +1,58 @@
-import React from 'react'
-import { images } from '../contants'
+import React, { useState } from 'react';
+import{ AiQutlineMenu,AiQutlineClose }
+import { images } from '../contants';
+
 const navItemInfo = [
-  {name:"Home"},
-  {name:"Articles"},
-  {name:"Pages"},
-  {name:"Pricing"},
-  {name:"Faq"},
+  { name: "Home" },
+  { name: "Articles" },
+  { name: "Pages" },
+  { name: "Pricing" },
+  { name: "Faq" },
 ];
-const NavItem = ({name}) => {
+
+const NavItem = ({ name }) => {
   return (
-    <li className=' relative group'>
-      <a href="/" className='px-4 py-2 '>
+    <li className='relative group'>
+      <a href="/" className='px-4 py-2'>
         {name}
       </a>
       <span className='text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100'>/</span>
     </li>
   );
 };
+
 const Header = () => {
+  const [navIsVisible, setIsVisible] = useState(false);
+  const navVisibilityHandler = () => {
+    setNavIsVisible((curState) => {
+      return !curState;
+    })
+  }
   return (
-    <div>
-      <section>
-        <header ClassName="container mx-auto px-5 flex justify-between py-4 items-center">
-          <div>
-            <img src={images.Logo} alt="logo" />
-          </div>
-          <div className='flex gap-x-9 items-center'>
-            <ul className='flex gap-x-5 font-semibold'>
+    <section>
+      <header className="container mx-auto px-5 flex justify-between py-4 items-center">
+        <div>
+          <img className='w-16' src={images.Logo} alt="logo" />
+        </div>
+        <div>
+          {navIsVisible  ? <AiQutlineClose  onClick={navVisibilityHandler}/>
+          : <AiQutlineMenu onClick={navVisibilityHandler}/>}
+          
+        </div>
+        <div className={' ${navIsVisible ? "right-0" : "-right-full"} flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0  lg:static gap-x-9 items-center'}>
+          <ul className='flex gap-x-5 font-semibold'>
             {navItemInfo.map((item) => (
               <NavItem key={item.name} name={item.name} />
+            )
+            )}
+          </ul>
+          <button className='border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300'>
+            sign in
+          </button>
+        </div>
+      </header>
+    </section>
+  );
+};
 
-            ))}
-           
-            </ul>
-            <button className='border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300'>
-              sing in
-            </button>
-
-          </div>
-        </header>
-      </section>
-    </div>
-  )
-}
-
-export default Header
+export default Header;
